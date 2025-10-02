74ecb4dfe46eccf33e62a3d62945a3288e3aa431
julio Rivas <juliorivasdev@gmail.com>
2025-10-02 00:29:53 -0300

    carga de unidadesDeMedida, departamento y provincia automaticamente al correr la app

--- src/main/java/com/example/demo/Domain/Repositories/RepoDepartamento.java
+++ src/main/java/com/example/demo/Domain/Repositories/RepoDepartamento.java
@@ -1,13 +1,16 @@
 package com.example.demo.Domain.Repositories;
 
 import com.example.demo.Domain.Entities.Departamento;
+import com.example.demo.Domain.Entities.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RepoDepartamento extends JpaRepository<Departamento, Long> {
 
     List<Departamento> findByProvinciaNombreIgnoreCase(String nombreProvincia);
+    Optional<Departamento> findByNombre(String nombre);
 }
